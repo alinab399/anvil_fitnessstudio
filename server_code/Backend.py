525254ad285dbc589f8cf0d6203606aa0411baf9
alina.bischof@student.htl-rankweil.at <alina.bischof@student.htl-rankweil.at>
2026-03-04 09:28:03 +0000

    Edited Startseite and Backend

--- server_code/Backend.py
+++ server_code/Backend.py
@@ -4,6 +4,7 @@ from anvil.tables import app_tables
 import anvil.files
 from anvil.files import data_files
 import anvil.server
+import sqlite3
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -17,5 +18,13 @@ import anvil.server
 #   print("Hello, " + name + "!")
 #   return 42
 #
+
+@anvil.server.callable
+def query_database_dict(query: str):
+  with sqlite3.connect(data_files["fitness_studio.db"]) as conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return [dict(row) for row in result]
 
 
